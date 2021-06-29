11b3e66d20d184ea90fccea8a16c04902e15e67b
gianni963 <gianni_guatieri@hotmail.com>
2021-06-29 06:03:06 -0700

    correction ex2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //année bissextile
-/*const annee = parseInt(prompt('Veuillez saisir une année !'));
+const annee = parseInt(prompt('Veuillez saisir une année !'));
 const resultatCotation = document.getElementById('resultat_cotation');
 if(isNaN(annee)){
     alert("veuillez entrer un nombre!")
@@ -15,35 +15,30 @@ else if((annee % 4 === 0 && annee % 100 > 0) || (annee % 400 === 0)) {
 const cotation = parseInt(prompt('Veuillez saisir une cotation !'));
 const resultatSwitch = document.getElementById('resultat_switch');
 
-switch (cotation) {
-    case 20: 
-        resultatSwitch.innerHTML = 'Excellent'; 
-        break;
-    case (cotation >= 17 && cotation < 19): 
-        resultatSwitch.innerHTML = 'Très bien'; 
-        break;
-    case (cotation >= 13 && cotation <= 16): 
-        resultatSwitch.innerHTML = 'Bien'; 
-        break;
-    case (cotation >= 10 && cotation <= 12): 
-        resultatSwitch.innerHTML = 'Suffisant'; 
-        break;
-    case (cotation == 8 || cotation == 9 ): 
-        resultatSwitch.innerHTML = 'insuffisant'; 
-        break;
-    case (cotation >= 0 && cotation <= 7): 
-        resultatSwitch.innerHTML = 'Echec'; 
-        break;
+if(cotation == 20){
+    resultatSwitch.innerHTML = 'Excellent'; 
 
-    default: 
-        // Les accolades sont optionnelles
-        if(isNaN(cotation)) 
+}else if(cotation >= 17 && cotation < 19){
+    resultatSwitch.innerHTML = 'Très bien'; 
+
+}else if(cotation >= 13 && cotation <= 16){
+    resultatSwitch.innerHTML = 'Bien';  
+}else if(cotation >= 10 && cotation <= 12){
+    resultatSwitch.innerHTML = 'Bien'; 
+}else if(cotation == 8 || cotation == 9 ){
+    resultatSwitch.innerHTML = 'insuffisant'; 
+
+}else if(cotation >= 0 && cotation <= 7){
+    resultatSwitch.innerHTML = 'Echec'; 
+}else{
+    if(isNaN(cotation)) 
             resultatSwitch.innerHTML = 'BOUMMMMMMMMMMMM!'; 
-        else 
+        else{
             resultatSwitch.innerHTML = 'Valeur en dehors du scope !'; 
-        break;
+    }
 }
-*/
+
+
 //date 
 const dateEncodee = prompt('encoder une date au format "DD-MM-YYYY"');
 regexDate = /^\d{2}-\d{2}-\d{4}$/;
